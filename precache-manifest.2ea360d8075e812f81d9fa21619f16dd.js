4525beb92823b9eb4f53e1b0e9ffa1b9516c92d9
yanchanga <937209904@qq.com>
2020-02-28 21:44:17 +0800

    deploy

---
+++ precache-manifest.2ea360d8075e812f81d9fa21619f16dd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74c2d2c337744f629b85",
+    "url": "/test-website/css/app.74447c06.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/test-website/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "191c488f97a371bad517ac1001f7100e",
+    "url": "/test-website/index.html"
+  },
+  {
+    "revision": "4dddd5f955de5ca4fc5b",
+    "url": "/test-website/js/about.1ccd927b.js"
+  },
+  {
+    "revision": "74c2d2c337744f629b85",
+    "url": "/test-website/js/app.1cdb9fd6.js"
+  },
+  {
+    "revision": "4a37e029aa9badcbf265",
+    "url": "/test-website/js/chunk-vendors.cd45d562.js"
+  },
+  {
+    "revision": "4dd2751dfa44c648270334e74aa5aa10",
+    "url": "/test-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/test-website/robots.txt"
+  }
+]);
